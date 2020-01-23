31d12bf9e9bfe719b220019712d7d694644ceb69
Patrick Villanueva <villanuv@yahoo.com>
2020-01-23 01:31:00 -0600

    refactored week#show and controller

--- app/models/shelver.rb
+++ app/models/shelver.rb
@@ -2,4 +2,12 @@ class Shelver < ActiveRecord::Base
 	has_many :schedules
 	has_many :daily_logs
 	has_many :cart_contents
+
+	def show_daily_log(year, week, day)
+		daily_logs.find_by(year: year, week_number: week, day_number: day)
+	end
+
+	def sched_for_day(year, week, day)
+		schedules.find_by(year: year, week_number: week).send(day)
+	end
 end
